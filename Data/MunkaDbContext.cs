aae418a39a189b69ec8c1ffcef26231f6e31ca57
Tóth Márton Marcell <hun.nort@gmail.com>
2023-12-11 11:47:58 +0100

    Data/ (connention to db)

---
+++ Data/MunkaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Autoszerelo.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Autoszerelo.Data
+{
+	public class MunkaDbContext : DbContext
+	{
+        public MunkaDbContext(DbContextOptions<MunkaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Munka> Munkak { get; set; }
+    }
+}
---
+++ Data/UgyfelDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Autoszerelo.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Autoszerelo.Data
+{
+	public class UgyfelDbContext : DbContext
+	{
+		public UgyfelDbContext(DbContextOptions<UgyfelDbContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Ugyfel> Ugyfelek { get; set; }
+	}
+}
